96de624fcf4284dfb8d2d8ae6f7d6cd87c5c150e
Bruno <bruno.rsilva2004@gmail.com>
2025-09-17 01:36:10 -0300

    teste para criação de superuser

--- accounts/management/commands/create_initial_superuser.py
+++ accounts/management/commands/create_initial_superuser.py
@@ -1,3 +1,5 @@
+# backend/accounts/management/commands/create_initial_superuser.py
+
 from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
 import os
@@ -8,17 +10,19 @@ class Command(BaseCommand):
     help = 'Cria um super utilizador de forma não-interativa usando variáveis de ambiente.'
 
     def handle(self, *args, **options):
-        username = os.environ.get('ADMIN_USER')
         email = os.environ.get('ADMIN_EMAIL')
         password = os.environ.get('ADMIN_PASS')
 
-        if not all([username, email, password]):
-            self.stdout.write(self.style.ERROR('As variáveis de ambiente ADMIN_USER, ADMIN_EMAIL, e ADMIN_PASS precisam de ser definidas.'))
+        if not all([email, password]):
+            self.stdout.write(self.style.ERROR('As variáveis de ambiente ADMIN_EMAIL e ADMIN_PASS precisam de ser definidas.'))
             return
 
-        if not User.objects.filter(username=username).exists():
-            self.stdout.write(self.style.SUCCESS(f'A criar super utilizador {username}...'))
-            User.objects.create_superuser(username=username, email=email, password=password)
+        if not User.objects.filter(email=email).exists():
+            self.stdout.write(self.style.SUCCESS(f'A criar super utilizador com o email {email}...'))
+            User.objects.create_superuser(
+                email=email,
+                password=password
+            )
             self.stdout.write(self.style.SUCCESS('Super utilizador criado com sucesso.'))
         else:
-            self.stdout.write(self.style.WARNING(f'Super utilizador {username} já existe.'))
+            self.stdout.write(self.style.WARNING(f'Super utilizador com o email {email} já existe.'))
