ea932f09cb2929e8bf7701930b6caf4a7992d09d
Bruno <bruno.rsilva2004@gmail.com>
2025-09-17 01:26:04 -0300

    debug true

--- balm/settings.py
+++ balm/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SETTINGS_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['balm.onrender.com']
 
