a0f1fa92e11b4f50649aec14fb498d0e922bce4e
Bruno <bruno.rsilva2004@gmail.com>
2025-09-18 21:54:30 -0300

    correções para teste

--- balm/settings.py
+++ balm/settings.py
@@ -157,7 +157,11 @@ EMAIL_HOST_USER = 'bruno.rsilva2004@gmail.com'
 EMAIL_HOST_PASSWORD = 'ktefrwmrpgltaspz'
 
 
-CORS_ALLOWED_ORIGINS = [True]
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "https://frontend-balm.vercel.app/",
+    ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
