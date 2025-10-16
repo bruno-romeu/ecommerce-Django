7f9a3e4a24593292f0264451e2a187d88c9875c6
Bruno <bruno.rsilva2004@gmail.com>
2025-10-15 22:33:25 -0300

    correções nas  urls

--- apis/site_config_api/urls.py
+++ apis/site_config_api/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .site_config_api_view import HeroSectionActivateView, HeroSectionDetailUpdateDestroyView, HeroSectionListCreateView, HeroSectionPublicView
 
 urlpatterns = [
-    path('api/hero/', HeroSectionPublicView.as_view(), name='hero-public'),
+    path('hero/', HeroSectionPublicView.as_view(), name='hero-public'),
     
-    path('api/admin/hero/', HeroSectionListCreateView.as_view(), name='hero-list-create'),
-    path('api/admin/hero/<int:pk>/', HeroSectionDetailUpdateDestroyView.as_view(), name='hero-detail'),
-    path('api/admin/hero/<int:pk>/activate/', HeroSectionActivateView.as_view(), name='hero-activate'),
+    path('admin/hero/', HeroSectionListCreateView.as_view(), name='hero-list-create'),
+    path('admin/hero/<int:pk>/', HeroSectionDetailUpdateDestroyView.as_view(), name='hero-detail'),
+    path('admin/hero/<int:pk>/activate/', HeroSectionActivateView.as_view(), name='hero-activate'),
 ]
