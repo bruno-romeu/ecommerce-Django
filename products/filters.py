fd197120f6ae93e75ddefa309c28af290f6ee98d
bruno <bruno.rsilva2004@gmail.com>
2025-10-17 17:16:46 -0300

    Alterações realizadas na API de pedidos para que seja possível que um usuário cancele o seu próprio pedido e também para que consulte um pedido com mais informações.

--- products/filters.py
+++ products/filters.py
@@ -5,8 +5,8 @@ class ProductFilterSet(filters.FilterSet):
     min_price = filters.NumberFilter(field_name="price", lookup_expr='gte')
     max_price = filters.NumberFilter(field_name="price", lookup_expr='lte')
 
-    category = filters.CharFilter(field_name='category__slug')
-    essence = filters.CharFilter(field_name='essence__slug')
+    category = filters.CharFilter(field_name='category__slug', lookup_expr='in')
+    essence = filters.CharFilter(field_name='essence__slug', lookup_expr='in')
 
     class Meta:
         model = Product
