427b2a7d7de04ca1d8c246aee57d08cde0b9077b
bruno <bruno.rsilva2004@gmail.com>
2025-10-31 11:30:53 -0300

    correção no middleware para receber os dados dos cookies no login.

--- apis/urls.py
+++ apis/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, include
+from apis.clients_api.clients_api_view import UserDetailView
 
 urlpatterns = [
     path('client/', include('apis.clients_api.urls')),
@@ -7,5 +8,7 @@ urlpatterns = [
     path('order/', include('apis.orders_api.urls')),
     path('checkout/', include('apis.checkout_api.urls')),
     path('site-config/', include('apis.site_config_api.urls')),
+    path('auth/users/me/', UserDetailView.as_view(), name='user_detail'),
+
 
 ]
