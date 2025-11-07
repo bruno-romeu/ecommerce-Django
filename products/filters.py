f639fb2ff99780d09f99528378a752426a5a5a84
bruno <bruno.rsilva2004@gmail.com>
2025-11-07 17:43:58 -0300

    correção no filters.py para aceitar mais de um filtro simultâneamente.

--- products/filters.py
+++ products/filters.py
@@ -1,12 +1,20 @@
 from django_filters import rest_framework as filters
-from .models import Product
+from .models import Product, Category, Essence
 
 class ProductFilterSet(filters.FilterSet):
     min_price = filters.NumberFilter(field_name="price", lookup_expr='gte')
     max_price = filters.NumberFilter(field_name="price", lookup_expr='lte')
 
-    category = filters.CharFilter(field_name='category__slug', lookup_expr='in')
-    essence = filters.CharFilter(field_name='essence__slug', lookup_expr='in')
+    category = filters.ModelMultipleChoiceFilter(
+        field_name='category__slug',
+        to_field_name='slug',
+        queryset=Category.objects.all()
+    )
+    essence = filters.ModelMultipleChoiceFilter(
+        field_name='essence__slug', 
+        to_field_name='slug',       
+        queryset=Essence.objects.all()
+    )
 
     class Meta:
         model = Product
