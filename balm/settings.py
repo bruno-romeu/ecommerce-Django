5f143c1752bba5f41ee1f1ce3c1c9d087a273dbd
Bruno <bruno.rsilva2004@gmail.com>
2025-11-09 15:08:17 -0300

    teste para correção de segurança de dados enviados ao backend.

--- balm/settings.py
+++ balm/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv("SETTINGS_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', default=False)
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(',')
+ALLOWED_HOSTS = ['balm.onrender.com', 'localhost', '127.0.0.1']
 
 
 
