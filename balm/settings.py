93088cb16b20d6df49fd4289e7290270cb474187
Bruno Romeu da Silva <bruno.rsilva2004@gmail.com>
2025-12-26 23:13:03 -0300

    update

--- balm/settings.py
+++ balm/settings.py
@@ -482,7 +482,7 @@ else:
             "BACKEND": "django.core.files.storage.FileSystemStorage",
         },
         "staticfiles": {
-            "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+            "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
         },
     }
 
