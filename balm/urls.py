77663edc448dfb09777725c4802a78d1a18d65af
bruno <bruno.rsilva2004@gmail.com>
2026-01-09 11:08:28 -0300

    update de correções.

--- balm/urls.py
+++ balm/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.views.decorators.csrf import csrf_exempt
 from inngest.django import serve
 from inngest_functions.send_verification_email import send_verification_email_fn
 from inngest_functions.process_shipping import process_shipping_fn
@@ -8,17 +7,14 @@ from ecommerce_inngest import inngest_client
 from django.conf import settings
 from django.conf.urls.static import static
 
-inngest_view = csrf_exempt(
-    serve(
-        client=inngest_client,
-        functions=[send_verification_email_fn, process_shipping_fn],
-    )
-)
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('apis.urls')),
-    path("api/inngest/", inngest_view),
+    serve(
+        path="api/inngest",  
+        client=inngest_client,
+        functions=[send_verification_email_fn, process_shipping_fn],
+    ),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
