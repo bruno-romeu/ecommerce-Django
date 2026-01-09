942a73656e975a8fea2abd3a4061b64a32c38550
bruno <bruno.rsilva2004@gmail.com>
2026-01-09 16:19:26 -0300

    update de correções.

--- ecommerce_inngest.py
+++ ecommerce_inngest.py
@@ -1,7 +1,9 @@
 import inngest
+import os
 
 inngest_client = inngest.Inngest(
     app_id="inngest_functions",
+    is_production=os.getenv("INNGEST_SIGNING_KEY")
 )
 
 __all__ = ['inngest_client']
