fd4a6768ea1d7eb33a430f37d4cc2097371ec904
bruno <bruno.rsilva2004@gmail.com>
2026-01-09 16:31:11 -0300

    update de correções.

--- ecommerce_inngest.py
+++ ecommerce_inngest.py
@@ -3,7 +3,7 @@ import os
 
 inngest_client = inngest.Inngest(
     app_id="inngest_functions",
-    is_production=os.getenv("INNGEST_SIGNING_KEY")
+    is_production=os.getenv("INNGEST_SIGNING_KEY") == "production",
 )
 
 __all__ = ['inngest_client']
--- inngest_functions/views.py
+++ inngest_functions/views.py
@@ -1,13 +1,15 @@
+from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from inngest.django import serve
 from ecommerce_inngest import inngest_client
 from .send_verification_email import send_verification_email_fn
 from .process_shipping import process_shipping_fn
 
+inngest_serve = serve(
+    inngest_client,
+    [send_verification_email_fn, process_shipping_fn],
+)
+
 @csrf_exempt
 def inngest_endpoint(request):
-    view = serve(
-        inngest_client,
-        [send_verification_email_fn, process_shipping_fn],
-    )
-    return view(request)
+    return inngest_serve(request)
