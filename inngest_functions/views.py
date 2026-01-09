5f8c16de250b45161d54beee4852962572ae3b94
bruno <bruno.rsilva2004@gmail.com>
2026-01-09 10:14:46 -0300

    update de correções.

--- inngest_functions/views.py
+++ inngest_functions/views.py
@@ -1,16 +1,13 @@
 from django.views.decorators.csrf import csrf_exempt
 from inngest.django import serve
 from ecommerce_inngest import inngest_client
+
 from .send_verification_email import send_verification_email_fn
 from .process_shipping import process_shipping_fn
 
 @csrf_exempt
 def inngest_endpoint(request):
-    """
-    Endpoint para o Inngest com as funções registradas
-    """
     return serve(
         inngest_client, 
-        [send_verification_email_fn, process_shipping_fn],
-        request
-    )
+        [send_verification_email_fn, process_shipping_fn]
+    )(request)
