4e29e8e0448792e086f0e8e9cf3053fab29b9800
bruno <bruno.rsilva2004@gmail.com>
2026-01-09 16:47:57 -0300

    update de correções.

--- balm/urls.py
+++ balm/urls.py
@@ -2,10 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from inngest_functions.urls import inngest_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/inngest/', include('inngest_functions.urls')),  # ← Mudança aqui
+    path('api/inngest/', inngest_view), 
     path('api/', include('apis.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
--- inngest_functions/urls.py
+++ inngest_functions/urls.py
@@ -1,12 +1,20 @@
-from django.urls import path
+from django.http import JsonResponse, HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_http_methods
 import inngest.django
 from ecommerce_inngest import inngest_client
 from .send_verification_email import send_verification_email_fn
 from .process_shipping import process_shipping_fn
 
-inngest.django.serve(
-    inngest_client, 
+handler = inngest.django.serve(
+    inngest_client,
     [send_verification_email_fn, process_shipping_fn]
 )
 
-urlpatterns = inngest.django.urls()
+@csrf_exempt
+@require_http_methods(["GET", "PUT", "POST"])
+def inngest_view(request):
+    """
+    View que processa requests do Inngest
+    """
+    return handler(request)
