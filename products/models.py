ed90fa7dcacdaf591e7ec080d781b6d50d17d1c0
Bruno Romeu da Silva <bruno.rsilva2004@gmail.com>
2026-01-13 21:29:56 -0300

    update

--- products/models.py
+++ products/models.py
@@ -54,7 +54,6 @@ class Size(models.Model):
     name = models.CharField(max_length=25, blank=True, null=True, verbose_name='Nome')
     weight = models.FloatField( blank=True, null=True, verbose_name='Peso')
     unit = models.CharField(max_length=2, choices=UNIDADES_DE_MEDIDA,
-                            default='g',
                             verbose_name='Unidade de Medida')
     height = models.FloatField(default=5.0, verbose_name='Altura (cm)')
     width = models.FloatField(default=3.0, verbose_name='Largura (cm)')
